d56a5d3ec06dce6b97983c1dcf8842704caf0a9e
anasmak04 <anasdev8@gmail.com>
2023-11-21 14:06:13 +0100

    Retrieve data from database to page web

--- views/user/show.php
+++ views/user/show.php
@@ -1,8 +1,8 @@
 <?php
 
-    include '../../database/DbConnection.php';
-    $sql = "SELECT * FROM user";
-    $result = $connexion->query($sql);
+include '../../database/DbConnection.php';
+$sql = "SELECT * FROM user";
+$result = $connexion->query($sql);
 
 ?>
 
@@ -11,6 +11,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,9 +19,48 @@
 
 
 </head>
+
 <body>
 
 
+    <table class="table">
 
+        <thead>
+
+            <tr>
+
+                <th>ID</th>
+
+                <th>First Name</th>
+
+                <th>Last Name</th>
+
+                <th>Email</th>
+
+                <th>Action</th>
+
+            </tr>
+
+        </thead>
+
+        <tbody>
+
+            <?php 
+
+                $r = $result->fetch_assoc();
+            ?>
+
+            <tr>
+            <td><?php echo $r['id']; ?></td>
+            <td><?php echo $r['firstName']; ?></td>
+            <td><?php echo $r['lastName']; ?></td>
+            <td><?php echo $r['email']; ?></td>
+            <td><a class="btn btn-info" href="edit.php?id=<?php echo $row['id']; ?>">Edit</a>
+            <td><a class="btn btn-info" href="delete.php?id=<?php echo $row['id']; ?>">Delete</a>
+        </tr>
+        </tbody>
+
+    </table>
 </body>
+
 </html>
